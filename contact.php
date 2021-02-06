9a085e8ad1e09278c9bd5fc165763a7e00b23c1a
undalwis <78288435+undalwis@users.noreply.github.com>
2021-02-06 13:36:39 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Email: $email \n Phone: $phone \n Message: $message";
-$recipient = "thomas@copy411.info";
+$recipient = "umeesha.dalwis@pepperdine.edu";
 $subject = "onestop.com Customer Contact";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error! Try again.");
