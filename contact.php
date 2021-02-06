acf807b2893854e10124ab1ac428ceff0ec7ffbf
undalwis <78288435+undalwis@users.noreply.github.com>
2021-02-06 13:36:11 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,9 +4,9 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Email: $email \n Phone: $phone \n Message: $message";
-$recipient = "umeesha.dalwis@pepperdine.edu";
+$recipient = "thomas@copy411.info";
 $subject = "onestop.com Customer Contact";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error! Try again.");
-echo "Thank You!" . " -" . "<a href='notguiltysurvey.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
